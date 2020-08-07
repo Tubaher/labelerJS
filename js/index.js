e66d2694e09865f52dedc070d9d91a6d801a4ddd
Tubaher <tubarao0705@gmail.com>
2020-08-07 12:23:46 -0500

    final arrange

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // import { saveAs } from 'file-saver';
 var cars = [];
-var directions = [];
+// var directions = [];
 var plates = [];
 var index = 1;
 
@@ -15,19 +15,19 @@ function nextImg() {
     // console.log(index)
     if (index <= cars.length){
 
-        var e = document.getElementById("direction");
-        var tmpDirection = e.options[e.selectedIndex].value;
+        // var e = document.getElementById("direction");
+        // var tmpDirection = e.options[e.selectedIndex].value;
 
         var tmpPlateNumber = document.getElementById("plateNumber").value;
     
-        directions.push(tmpDirection);
+        // directions.push(tmpDirection);
         plates.push(tmpPlateNumber.concat("\n"));
 
         document.getElementById("plateNumber").value = "";
         if (index == cars.length){
             document.getElementById("saveBtn").disabled = false;
             document.getElementById("nextBtn").disabled = true;
-            document.getElementById("direction").disabled = true;
+            // document.getElementById("direction").disabled = true;
             document.getElementById("plateNumber").disabled = true;
         }
     }
@@ -59,7 +59,8 @@ function saveStaticDataToFile() {
 }
 
 function joinNumberAndPlate(num, idx) {
-    return num + " <" +  directions[idx] + ">"+plates[idx];
+    // return num + " <" +  directions[idx] + ">"+plates[idx];
+    return num + " " + plates[idx];
 }  
 
 
